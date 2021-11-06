af17e6c99a5a65108418712dd78dfd45a271d25e
Takahashi <Takahashi>
2021-11-06 20:54:49 +0900

    first commit

--- OshitakuApp/ViewController.swift
+++ OshitakuApp/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        DateLabel.text = getToday(format: "yyyy年MM月dd日")
     }
+    
+    func getToday(format:String = "yyyy/MM/dd HH:mm:ss") -> String{
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: now as Date)
+    }
+    
 
 
 }
